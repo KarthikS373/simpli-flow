e59237dae091c122aa45c90c2afd60509f7b02db
Advit <lci2021045@iiitl.ac.in>
2023-07-11 00:43:46 +0530

    Changed Prompt

--- src/generate.py
+++ src/generate.py
@@ -10,8 +10,9 @@ load_dotenv()
 # Set your OpenAI API key here
 openai.api_key = os.getenv('API_KEY')
 
+
 def generate_code(description):
-    description_fin = "\nYou are an expert Smart Contract Developer. Generate Code for the following Smart Contract in Cadence Language."+description 
+    description_fin = "\nYou are an expert in developing Smart Contracts in the Cadence Language. Generate Code for the following Smart Contract:"+description 
     response = openai.Completion.create(
         engine='text-davinci-003',
         prompt=description_fin,
